c8bb92eef21aa344176c682657fec5853e6b5fcf
whaoa <whaoa.w@outlook.com>
2024-11-19 18:27:55 +0800

    chore: add stylelint support

---
+++ stylelint.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-vue',
+    'stylelint-config-hudochenkov/full',
+  ],
+  rules: {
+    'selector-max-id': null,
+    'selector-max-type': null,
+    'selector-no-qualifying-type': null,
+    'value-keyword-case': null,
+    'alpha-value-notation': ['number'],
+    'color-function-notation': ['legacy'],
+    'custom-property-empty-line-before': null,
+  },
+};
